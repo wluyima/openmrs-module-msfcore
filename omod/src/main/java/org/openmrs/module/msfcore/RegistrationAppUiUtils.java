fdd0ca403067a27d80d62dbe66529c17f48d0b24
lauravignoli <vignoli.laura@gmail.com>
2018-09-04 15:00:58 +0200

    MOO-239 Enable dateOfArrival editing

--- omod/src/main/java/org/openmrs/module/msfcore/RegistrationAppUiUtils.java
+++ omod/src/main/java/org/openmrs/module/msfcore/RegistrationAppUiUtils.java
@@ -1,24 +1,27 @@
 package org.openmrs.module.msfcore;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Set;
 
 import org.openmrs.Patient;
 import org.openmrs.PatientIdentifier;
-import org.openmrs.Person;
 import org.openmrs.PersonAddress;
 import org.openmrs.PersonAttribute;
 import org.openmrs.api.context.Context;
 
 public class RegistrationAppUiUtils {
     /**
-     * Gets the person attribute value for the specified person for the
+     * Gets the person attribute value for the specified patient for the
      * getPersonAttributeTypeByUuid that matches the specified uuid
      * 
      * @return the attribute value
      */
-    public String getAttribute(Person person, String attributeTypeUuid) {
-        if (person != null) {
-            PersonAttribute attr = person.getAttribute(Context.getPersonService().getPersonAttributeTypeByUuid(attributeTypeUuid));
+    public String getAttribute(Patient patient, String attributeTypeUuid) {
+        if (patient != null) {
+            PersonAttribute attr = patient.getAttribute(
+                    Context.getPersonService().getPersonAttributeTypeByUuid(attributeTypeUuid));
             if (attr != null) {
                 return attr.getValue();
             }
@@ -45,6 +48,16 @@ public class RegistrationAppUiUtils {
             return patient.getAddresses();
         } else {
             return null;
+        }
+    }
+
+    public Date getDateOfArrival(Patient patient) {
+        String date = getAttribute(patient, MSFCoreConfig.PERSON_ATTRIBUTE_DATE_OF_ARRIVAL_UUID);
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-M-dd");
+        try {
+            return date != null ? format.parse(date) : new Date();
+        } catch (ParseException e) {
+            return new Date();
         }
     }
 }
---
+++ omod/src/main/webapp/fragments/field/dateOfArrival.gsp
@@ -0,0 +1,1 @@
+${ ui.includeFragment("uicommons", "field/multipleInputDate", [initialValue: ui.escapeAttribute(uiUtils.getDateOfArrival(patient) ] << config) }
