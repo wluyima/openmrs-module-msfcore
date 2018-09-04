6b703ddee11324888ebcc17ec7cff6fcb1cadc8d
k-joseph <kaweesi@jembi.org>
2018-09-04 23:34:55 +0300

    MOO-292: only log login event from login page and junit

--- api/src/main/java/org/openmrs/module/msfcore/listener/LogInOrOutEventListener.java
+++ api/src/main/java/org/openmrs/module/msfcore/listener/LogInOrOutEventListener.java
@@ -1,6 +1,8 @@
 package org.openmrs.module.msfcore.listener;
 
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 import org.openmrs.User;
 import org.openmrs.UserSessionListener;
@@ -19,16 +21,35 @@ public class LogInOrOutEventListener implements UserSessionListener {
                             Context.getMessageSourceService().getMessage("msfcore.logInOrOut", new Object[]{user.getUsername(), status},
                                             null)).date(new Date()).build();
             if (UserSessionListener.Event.LOGIN.equals(event)) {
-                logInOrOut.setEvent(AuditLog.Event.LOGIN);
-                if (status.equals(Status.SUCCESS)) {
-                    logInOrOut.setUser(user);
+                if (acceptableToLogLogin()) {
+                    logInOrOut.setEvent(AuditLog.Event.LOGIN);
+                    if (status.equals(Status.SUCCESS)) {
+                        logInOrOut.setUser(user);
+                    }
                 }
             } else if (UserSessionListener.Event.LOGOUT.equals(event)) {
                 logInOrOut.setEvent(AuditLog.Event.LOGOUT);
                 logInOrOut.setUser(user);
             }
-            Context.getService(AuditService.class).saveAuditLog(logInOrOut);
+            if (logInOrOut.getEvent() != null) {
+                Context.getService(AuditService.class).saveAuditLog(logInOrOut);
+            }
         }
     }
 
+    /**
+     * login should come from a web user login except for unit tests
+     * 
+     * @return canLogLogin event
+     */
+    private boolean acceptableToLogLogin() {
+        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+        List<StackTraceElement> list = Arrays.asList(stackTrace);
+        for (StackTraceElement element : list) {
+            if (element.getClassName().startsWith("org.junit.") || element.getClassName().endsWith("LoginPageController")) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
