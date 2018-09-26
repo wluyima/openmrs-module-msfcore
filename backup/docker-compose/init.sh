8f0e094ea9f1995f962bc0d2455c30e17bdbdb67
Edrisse Mussá <31036743+edrisse@users.noreply.github.com>
2018-09-26 16:23:20 +0200

    Update init.sh

--- backup/docker-compose/init.sh
+++ backup/docker-compose/init.sh
@@ -6,7 +6,6 @@ rm docker-compose*
 wget https://raw.githubusercontent.com/edrisse/openmrs-module-msfcore/master/backup/docker-compose/docker-compose.yml
 wget https://raw.githubusercontent.com/edrisse/openmrs-module-msfcore/master/backup/docker-compose/docker-compose.override.yml
 wget https://raw.githubusercontent.com/edrisse/openmrs-module-msfcore/master/backup/docker-compose/docker-compose.prod.yml
-wget https://raw.githubusercontent.com/edrisse/openmrs-module-msfcore/master/backup/docker-compose/zip_file_password.txt
 echo $1 > zip_file_password.txt
 mkdir backup
 else
