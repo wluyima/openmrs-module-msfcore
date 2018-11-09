6a9ce4feee96e55b315bab2ba701b20f0990b46d
Tresor <tresor.mvumbi@jembi.org>
2018-11-09 15:38:41 +0200

    MOO-675 Remove retired records from the appointment list drop-down
    This commit excludes the appointment types that are retired from the drop-down

--- omod/src/main/java/org/openmrs/module/msfcore/fragment/controller/field/AppointmentListFragmentController.java
+++ omod/src/main/java/org/openmrs/module/msfcore/fragment/controller/field/AppointmentListFragmentController.java
@@ -2,7 +2,6 @@ package org.openmrs.module.msfcore.fragment.controller.field;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 import org.openmrs.api.context.Context;
 import org.openmrs.module.appointmentscheduling.AppointmentType;
@@ -11,8 +10,9 @@ import org.openmrs.module.msfcore.DropDownFieldOption;
 import org.openmrs.ui.framework.page.PageModel;
 
 public class AppointmentListFragmentController {
-    public void controller(PageModel model) {
-		Set<AppointmentType> appointments = Context.getService(AppointmentService.class).getAllAppointmentTypes();
+	
+	public void controller(PageModel model) {
+		List<AppointmentType> appointments = Context.getService(AppointmentService.class).getAllAppointmentTypes(false);
 		List<DropDownFieldOption> options = new ArrayList<>();
 		for (AppointmentType appointment : appointments) {
 			DropDownFieldOption option = new DropDownFieldOption(appointment.getUuid(), appointment.getName());
